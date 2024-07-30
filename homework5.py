2bb083c9ffdeebfbda4003dc49c246bea6ae93e2
ohseoyeon <streamoh1111@gmail.com>
2024-07-30 10:32:23 +0900

    리뷰

--- homework5.py
+++ homework5.py
@@ -5,12 +5,9 @@ from scipy.stats import uniform
 
 x = uniform.rvs(loc=3, scale=7, size=20*10000).reshape(-1, 20)
 
-s_2 = x.var(axis=1, ddof=1)
-sns.histplot(s_2, stat="density")
+s_2 = x.var(axis=1, ddof=1) 
+sns.histplot(s_2, stat="density", color="red")
 plt.axvline(x=np.var(x), color='green', linestyle='-', linewidth=2)
-plt.show()
-plt.clf()
-
 k_2 = x.var(axis=1, ddof=0)
 sns.histplot(k_2, stat="density")
 plt.axvline(x=np.var(x), color='green', linestyle='-', linewidth=2)
